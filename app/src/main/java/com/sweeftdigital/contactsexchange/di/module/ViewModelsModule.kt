fd060430762a7d156a4b06cda2b614711ae8b5d5
Archil Asanishvili <arch3r_temp@yahoo.com>
2024-12-18 00:44:04 +0400

    Upgrade Koin up to 4.0.0

--- app/src/main/java/com/sweeftdigital/contactsexchange/di/module/ViewModelsModule.kt
+++ app/src/main/java/com/sweeftdigital/contactsexchange/di/module/ViewModelsModule.kt
@@ -5,7 +5,7 @@ import com.sweeftdigital.contactsexchange.presentation.card.CardViewModel
 import com.sweeftdigital.contactsexchange.presentation.create.CreateCardViewModel
 import com.sweeftdigital.contactsexchange.presentation.edit.EditCardViewModel
 import com.sweeftdigital.contactsexchange.presentation.home.HomeViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val VIEW_MODELS_MODULE = module {
